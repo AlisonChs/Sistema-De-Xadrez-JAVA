9f60f14460c7e059ea0a10c706c72ada055aea51
Alison Christian <alison.carvalho@etec.sp.gov.br>
2020-04-06 18:18:36 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,8 +26,9 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(3, 4));
+		board.placePiece(new King(board, Color.BLACK), new Position(2, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(1, 4));
 	}
 	
 }
